72ad192c08545ec47808029b7803fe74ca0016cd
unknown <maxdelafuente1599@gmail.com>
2022-04-05 00:07:31 -0500

    Location page terminada

--- src/components/Filters/Category/InputGroup.js
+++ src/components/Filters/Category/InputGroup.js
@@ -6,7 +6,7 @@ const InputGroup = ({total, name, setID}) => {
         <select 
         onChange={e=>setID(e.target.value)}
         class="form-select" id={name}>
-            <option selected>Choose...</option>
+            <option value = "1" selected>Choose...</option>
             {[...Array(total).keys()].map(x=>{
               return (
                 <option value= {x + 1}>
